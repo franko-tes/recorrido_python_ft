341c31ca70526cf2cd98edd1d0d991b5e8503fc6
Franco Teseyra <fteseyra@hotmail.com>
2025-01-24 19:25:39 -0300

    semantica, sintaxis, variables, palabras reservadas

---
+++ 01.inicio.py
@@ -0,0 +1,26 @@
+#Podemos usar Python desde la terminal.
+#Si usas WSL escribe "python3".
+#Luego escribe el tipico "Hola mundo".
+print("Hola mundo") 
+#El resultado saldra en pantalla.
+
+
+print(8+8) #funcion print-imprimir.
+
+#Semantica y Sintaxis
+
+#Sintaxis: la manera en que seguimos las buenas practicas al escribir codigo.
+print("Buen")
+print("dia")
+#La identacion es importante. O tendremos error de sintaxis.
+
+#Semantica: el sentido que le damos al codigo.
+#Variables: nombre, tipo y dato.
+saludo = "Como estas?"
+saludo = "Hello"
+nombre = "Hernan" #no podemos iniciar variables con numeros
+print(saludo)
+print(nombre)
+
+#palabras reservadas: son propias del lenguaje y tienen un color diferente.
+#no se pueden usar para nombrar variables.
