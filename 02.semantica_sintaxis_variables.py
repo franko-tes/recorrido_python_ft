ea564688ea1c80bc7f6f1652afcb7298deecfd53
Franco Teseyra <fteseyra@hotmail.com>
2025-01-24 19:57:36 -0300

    se creo archivo 02

---
+++ 02.semantica_sintaxis_variables.py
@@ -0,0 +1,17 @@
+#Semantica y Sintaxis
+
+#Sintaxis: la manera en que seguimos las buenas practicas al escribir codigo.
+print("Buen")
+print("dia")
+#La identacion es importante. O tendremos error de sintaxis.
+
+#Semantica: el sentido que le damos al codigo.
+#Variables: nombre, tipo y dato.
+saludo = "Como estas?"
+saludo = "Hello"
+nombre = "Hernan" #no podemos iniciar variables con numeros
+print(saludo)
+print(nombre)
+
+#palabras reservadas: son propias del lenguaje y tienen un color diferente.
+#no se pueden usar para nombrar variables.
