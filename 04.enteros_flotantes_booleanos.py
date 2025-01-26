6deb6fe9cca65e31fd3d68d9985b42d25ae276a6
Franco Teseyra <fteseyra@hotmail.com>
2025-01-26 19:00:06 -0300

    se creo archivo 04.enteros-flotantes-booleanos

---
+++ 04.enteros_flotantes_booleanos.py
@@ -0,0 +1,32 @@
+print("--Enteros--")
+
+x = 10
+print(x)
+print(type(x))
+
+print("--Flotantes--")
+
+y = 5.678 #numero flotante
+print(y)
+print(type(y))
+
+print("--Notacion Cientifica--")
+#para numeros muy grandes o muy chicos 
+z = 1.2e6
+print("1.2e6")
+print(z)
+print(type(z))
+
+print("--operaciones--")
+
+print("x+x:", x+x)
+print("x+y:", x+y)
+print("y+y:", y+y)
+
+print("--Booleanos--")
+
+is_true = True #la "T" debe ir en mayuscula, es una palabra reservada
+is_false = False
+
+print(is_true)
+print(type(is_true))
