51409ee604db91af1a497f9e8e68da67a4f2fc25
Franco Teseyra <fteseyra@hotmail.com>
2025-02-05 12:07:24 -0300

    contenido archivo 06.operaciones

---
+++ 06.operaciones.py
@@ -0,0 +1,45 @@
+print("\n--Operaciones--")
+a = 10
+b = 3
+print("\na=10 y b=3")
+print("\nSuma: a + b =", a + b)
+print("Resta: a - b =", a - b)
+print("Multiplicación: a * b =", a * b)
+print("Potenciacion: a ** b=", a ** b)
+print("División: a / b =", a / b)
+print("Parte entera de la division: a // b=", a //b)
+print("Módulo: a % b =", a % b)
+
+print("\n--Atajo--")
+a = a + 2
+print("La operación: a = a + 2 =", a)
+c = 10
+c += 2
+print("Es igual que: a += 2 =", c)
+
+print("\n--Busca la regla de PEMDAS--")
+print("a=2, b=3, c=4")
+operation_1 = 2 + 3 * 4
+operation_2 = 2 + (3 * 4)
+print("a + b * c =", operation_1)
+print("a + (b * c) =", operation_2)
+operation_3 = (2 + 3) * 4
+print("(a + b) * c =", operation_3)
+operation_4 = (2+3) * (4**2) / 8 - 1
+print("(a + b) * (c ** a) / 8 - 1 =", operation_4)
+
+print("\n--Booleanos--")
+a = 10
+b = 3
+c = 10.0
+d = "10"
+print("a=10, b=3, c=10.0, d=Diez")
+print("a > b", a > b)
+print("a < b", a < b)
+print("a >= b", a >= b)
+print("a <= b", a <= b)
+print("a >= c", a >= c)
+print("a == b", a == b)
+print("a == d", a == d)
+print("a != b", a != b)
+
