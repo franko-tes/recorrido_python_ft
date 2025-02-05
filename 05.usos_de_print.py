a001e106425e1ee6f4f45e7d77b78e42937eee1e
Franco Teseyra <fteseyra@hotmail.com>
2025-02-05 10:55:30 -0300

    se emprolijo archivo 05

--- 05.usos_de_print.py
+++ 05.usos_de_print.py
@@ -1,14 +1,14 @@
-print("--01.Uso basico--")
+print("\n--01.Uso basico--")
 print("Pondremos entre comillas simples, dobles o triples lo que queremos que se imprima en pantalla.")
 print('''Ejemplo: print("prueba")''')
 print("Salida: prueba")
 
-print("--02.Uso de la coma--")
+print("\n--02.Uso de la coma--")
 print("Usamos la coma para separar argumentos y Python añade un espacio.")
 print('''Ejemplo: print("Hoy", "es", "domingo")''')
 print("Salida:", "Hoy", "es", "domingo")
 
-print("--03.Uso del + --")
+print("\n--03.Uso del + --")
 print("Usamos el signo + para concatenar pero no se agrega el espacio.")
 print('''Ejemplo: print("Hoy" + "es" + "domingo")''')
 print("Salida:", "Hoy" + "es" + "domingo")
@@ -16,12 +16,12 @@ print("Debemos agregar nosotros el espacio")
 print('''Ejemplo: print("Hoy" + " " + "es" + " " + "domingo")''')
 print("Salida:", "Hoy" + " " + "es" + " " + "domingo")
 
-print("--04.Uso de sep--")
+print("\n--04.Uso de sep--")
 print("El parametro sep permite especificar como separar los elementos.")
 print('''Ejemplo: print("Hoy", "es", "domingo", sep="; ")''')
 print("Salida: Hoy", "es", "domingo", sep="; ")
 
-print("--05.Uso de end--")
+print("\n--05.Uso de end--")
 print("Print por defecto agrega un salto de linea. Con el uso de END podemos evitarlo o definir lo que se imprimira al final de la linea.")
 print('''Ejemplo: print("Mañana", end=" ")
 
@@ -29,7 +29,7 @@ print("lunes")''')
 print("Salida: Mañana",  end=" ")
 print("lunes")
 
-print("--06.Impresion de variables--")
+print("\n--06.Impresion de variables--")
 print("Mostraremos el valor de una variables.")
 print('''Ejemplo: 
 año = 2025
@@ -39,7 +39,7 @@ año = 2025
 mes = "enero"
 print("Salida:", año, mes)
 
-print("--07.Uso de formato con f-strings--")
+print("\n--07.Uso de formato con f-strings--")
 print("Podemos insertar expresiones dentro de cadenas de texto.")
 print('''Ejemplo:
 mes = enero
@@ -49,7 +49,7 @@ mes = "enero"
 año = 2025
 print(f"Salida: Mes: {mes}, Año: {año}")
 
-print("--08.Uso de formato con format--")
+print("\n--08.Uso de formato con format--")
 print("Usando {} como marcadores de posición, puedes pasar los valores que quieres insertar como argumentos de format.")
 print('''Ejemplo:
 mes = enero
@@ -59,7 +59,7 @@ mes = "enero"
 año = 2025
 print("Salida: Mes: {}, Año: {}".format(mes, año))
 
-print("--08.Impresion con formato especifico--")
+print("\n--09.Impresion con formato especifico--")
 print("Podemos indicar la cantidad de decimales a imprimir.")
 print('''Ejemplo:
 valor = 3.14159
@@ -67,7 +67,7 @@ print("Valor: {:.2f}".format(valor))''')
 valor = 3.14159
 print("Salida: Valor: {:.2f}".format(valor))
 
-print("--09.Salida de linea y caracteres especiales--")
+print("\n--10.Salida de linea y caracteres especiales--")
 print("A.Hacer saltos de linea con \\n")
 print('''Ejemplo: print("Hola\\nmundo")''')
 print("Salida: Hola\nmundo")
