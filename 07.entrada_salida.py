b6e2e224ca97a4d1325b37ba4f5165ad8414c53c
Franco Teseyra <fteseyra@hotmail.com>
2025-02-05 12:38:58 -0300

    contenido 07.entrada_salida

---
+++ 07.entrada_salida.py
@@ -0,0 +1,12 @@
+print("\n--Entrada y salida en consola--")
+
+nombre = input("Nombre:")
+print(nombre)
+print(type(nombre))
+edad = input("Edad:")
+print(edad)
+print(type(edad)) #lo que se ingrese por input sera tipo string
+
+edad = int(input("Edad:")) #usamos int para pasar los numeros a entero. o float para flotante.
+print(edad)
+print(type(edad))
