27479709993e76ccb85efffe08258216a2e9c0a3
Franco Teseyra <fteseyra@hotmail.com>
2025-07-13 19:43:48 -0300

    se emprolijó 04

--- 04.enteros_flotantes_booleanos.py
+++ 04.enteros_flotantes_booleanos.py
@@ -3,12 +3,14 @@ print("--Enteros--")
 x = 10
 print(x)
 print(type(x))
+print("\n")
 
 print("--Flotantes--")
 
 y = 5.678 #numero flotante
 print(y)
 print(type(y))
+print("\n")
 
 print("--Notacion Cientifica--")
 #para numeros muy grandes o muy chicos 
@@ -16,12 +18,14 @@ z = 1.2e6
 print("1.2e6")
 print(z)
 print(type(z))
+print("\n")
 
 print("--operaciones--")
 
 print("x+x:", x+x)
 print("x+y:", x+y)
 print("y+y:", y+y)
+print("\n")
 
 print("--Booleanos--")
 
