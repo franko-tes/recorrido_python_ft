bcc689d49b6ec671845b70b9754bd3f2c2d91968
Franco Teseyra <fteseyra@hotmail.com>
2025-07-16 20:22:10 -0300

    contenido 08.listas

---
+++ 08.listas.py
@@ -0,0 +1,8 @@
+#las listas almacenan la informacion entre corchetes.
+#por ejemplo, si armamos una lista de viaje, se veria asi:
+
+to_do = ["llegar al hotel",
+         "almorzar",
+         "visitar un museo",
+         "regresar al hotel"]
+print(to_do)
